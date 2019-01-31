ed9369c346c1ba94a3ac3ddba61f81d2bd34b2c6
Jardi Martinez <1088732+jardiamj@users.noreply.github.com>
2019-01-31 09:54:50 -0800

    Added disclaimer

--- byows_rpi.py
+++ byows_rpi.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
-#
-# Copyright 2018 Jardi A. Martinez Jordan <jardiamj@gmail.com>
-# See the file LICENSE.txt for your rights.
-# This is an weeWX driver implementation of the Build Your OWN Weather
-# Station usin the Raspberry Pi: 
-# https://projects.raspberrypi.org/en/projects/build-your-own-weather-station/
-
 """
-Driver for Build Your Own Weather Station (BYOWS) with a Raspberry Pi.
+Copyright 2018 Jardi A. Martinez Jordan <jardiamj@gmail.com>
+
+This is an weeWX driver implementation of the Build Your OWN Weather
+Station usin the Raspberry Pi: 
+https://projects.raspberrypi.org/en/projects/build-your-own-weather-station/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 
