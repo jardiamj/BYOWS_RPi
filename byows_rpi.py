16cb4aa1b6393aefd19ccd868265fdb7932efae3
Jardi A. Martinez Jordan <1088732+jardiamj@users.noreply.github.com>
2019-08-28 17:06:35 -0700

    Formatted code using black

--- byows_rpi.py
+++ byows_rpi.py
@@ -33,25 +33,31 @@ import smbus2
 
 import weewx.drivers
 
-DRIVER_NAME = 'BYOWS'
-DRIVER_VERSION = '0.51'
+DRIVER_NAME = "BYOWS"
+DRIVER_VERSION = "0.51"
+
 
 def loader(config_dict, _):
     return ByowsRpi(**config_dict[DRIVER_NAME])
+
 
 """
 def confeditor_loader():
     return ByowsRpiConfEditor()
 """
 
+
 def logmsg(level, msg):
-    syslog.syslog(level, 'BYOWS RPi: %s' % msg)
+    syslog.syslog(level, "BYOWS RPi: %s" % msg)
+
 
 def logdbg(msg):
     logmsg(syslog.LOG_DEBUG, msg)
 
+
 def loginf(msg):
     logmsg(syslog.LOG_INFO, msg)
+
 
 def logerr(msg):
     logmsg(syslog.LOG_ERR, msg)
@@ -61,20 +67,21 @@ class ByowsRpi(weewx.drivers.AbstractDevice):
     """weewx driver for the Build Your Own Weather Station - Raspberry Pi
 
     """
+
     def __init__(self, **stn_dict):
-        self.hardware = stn_dict.get('hardware', "BYOWS - Raspberry Pi")
-        self.loop_interval = float(stn_dict.get('loop_interval', 5))
+        self.hardware = stn_dict.get("hardware", "BYOWS - Raspberry Pi")
+        self.loop_interval = float(stn_dict.get("loop_interval", 5))
         params = dict()
-        params['anem_pin'] = int(stn_dict.get('anemometer_pin', 5))
-        params['rain_bucket_pin'] = int(stn_dict.get('rain_bucket_pin', 6))
-        params['bme280_port'] = int(stn_dict.get('bme280_port', 1))
-        params['bme280_address'] = int(stn_dict.get('bme280_address', 0x77), 16)
-        params['mcp3008_channel'] = int(stn_dict.get('mcp3008_channel', 0))
-        params['anem_adjustment'] = float(stn_dict.get('anemometer_adjustment', 1.18))
-        params['bucket_size'] = float(stn_dict.get('bucket_size', 0.2794))
-        params['anem_radius_cm'] = float(stn_dict.get('anemometer_radius_cm', 9.0))
-        loginf('using driver %s' % DRIVER_NAME)
-        loginf('driver version is %s' % DRIVER_VERSION)
+        params["anem_pin"] = int(stn_dict.get("anemometer_pin", 5))
+        params["rain_bucket_pin"] = int(stn_dict.get("rain_bucket_pin", 6))
+        params["bme280_port"] = int(stn_dict.get("bme280_port", 1))
+        params["bme280_address"] = int(stn_dict.get("bme280_address", 0x77), 16)
+        params["mcp3008_channel"] = int(stn_dict.get("mcp3008_channel", 0))
+        params["anem_adjustment"] = float(stn_dict.get("anemometer_adjustment", 1.18))
+        params["bucket_size"] = float(stn_dict.get("bucket_size", 0.2794))
+        params["anem_radius_cm"] = float(stn_dict.get("anemometer_radius_cm", 9.0))
+        loginf("using driver %s" % DRIVER_NAME)
+        loginf("driver version is %s" % DRIVER_VERSION)
         self.station = ByowsRpiStation(**params)
 
     @property
@@ -85,32 +92,34 @@ class ByowsRpi(weewx.drivers.AbstractDevice):
         """ Function that generates packets for weeWX by looping through station
         data generator function. """
         while True:
-            packet = {'dateTime': int(time.time() + 0.5),
-                    'usUnits': weewx.METRIC}
+            packet = {"dateTime": int(time.time() + 0.5), "usUnits": weewx.METRIC}
             data = self.station.get_data()
             packet.update(data)
             yield packet
-            time.sleep(self.loop_interval) # defaults to 5 seconds
+            time.sleep(self.loop_interval)  # defaults to 5 seconds
 
 
 class ByowsRpiStation(object):
     """ Object that represents a BYOWS_Station. """
+
     CM_IN_A_KM = 100000.0
     SECS_IN_AN_HOUR = 3600
+
     def __init__(self, **params):
         """ Initialize Object. """
-        self.bme280_address = params.get('bme280_address')
-        self.bme280_bus = smbus2.SMBus(params.get('bme280_port'))
+        self.bme280_address = params.get("bme280_address")
+        self.bme280_bus = smbus2.SMBus(params.get("bme280_port"))
         self.bme280_sensor = bme280
-        self.bme280_sensor.load_calibration_params(self.bme280_bus,
-                                                   self.bme280_address)
-        self.bucket_size = params.get('bucket_size') # in mm
+        self.bme280_sensor.load_calibration_params(self.bme280_bus, self.bme280_address)
+        self.bucket_size = params.get("bucket_size")  # in mm
         self.rain_count = 0
-        self.wind_gauge = WindGauge(params.get('mcp3008_channel'),
-                                    params.get('anem_pin'),
-                                    params.get('anem_radius_cm'),
-                                    params.get('anem_adjustment'))
-        self.rain_sensor = Button(params.get('rain_bucket_pin'))
+        self.wind_gauge = WindGauge(
+            params.get("mcp3008_channel"),
+            params.get("anem_pin"),
+            params.get("anem_radius_cm"),
+            params.get("anem_adjustment"),
+        )
+        self.rain_sensor = Button(params.get("rain_bucket_pin"))
         self.rain_sensor.when_pressed = self.bucket_tipped
         self.temp_probe = DS18B20()
 
@@ -119,8 +128,10 @@ class ByowsRpiStation(object):
 
     def get_bme280_data(self):
         try:
-            data = self.bme280_sensor.sample(self.bme280_bus,self.bme280_address)
-            humidity, pressure, temperature = data.humidity, data.pressure, data.temperature
+            data = self.bme280_sensor.sample(self.bme280_bus, self.bme280_address)
+            humidity = data.humidity
+            pressure = data.pressure
+            temperature = data.temperature
         except:
             logdbg("Error sampling sensor bme280, passing None as data.")
             humidity, pressure, temperature = None, None, None
@@ -132,30 +143,31 @@ class ByowsRpiStation(object):
 
     def get_rainfall(self):
         """ Returns rainfall in cm. """
-        rainfall = (self.rain_count * self.bucket_size)/10.0
+        rainfall = (self.rain_count * self.bucket_size) / 10.0
         self.reset_rainfall()
         return rainfall
 
     def get_data(self):
         """ Generates data packets every time interval. """
         data = dict()
-        anem_rotations = self.wind_gauge.wind_count/2.0
+        anem_rotations = self.wind_gauge.wind_count / 2.0
         time_interval = self.wind_gauge.last_wind_time - time.time()
         wind_speed, wind_dir = self.wind_gauge.get_wind()
         humidity, pressure, ambient_temp = self.get_bme280_data()
-        data['outHumidity'] = humidity
-        data['pressure'] = pressure
-        data['outTemp'] = ambient_temp
-        data['soilTemp1'] = self.get_soil_temp()
-        data['windSpeed'] = float(wind_speed)
-        data['windDir'] = wind_dir
-        data['rain'] = float(self.get_rainfall())
-        data['anemRotations'] = anem_rotations
-        data['timeAnemInterval'] = time_interval
+        data["outHumidity"] = humidity
+        data["pressure"] = pressure
+        data["outTemp"] = ambient_temp
+        data["soilTemp1"] = self.get_soil_temp()
+        data["windSpeed"] = float(wind_speed)
+        data["windDir"] = wind_dir
+        data["rain"] = float(self.get_rainfall())
+        data["anemRotations"] = anem_rotations
+        data["timeAnemInterval"] = time_interval
         return data
 
     def reset_rainfall(self):
         self.rain_count = 0
+
 
 class DS18B20(object):
     """
@@ -163,9 +175,10 @@ class DS18B20(object):
     w1-gpio
     w1-therm
     """
+
     def __init__(self):
         w1_devices = glob.glob("/sys/bus/w1/devices/28*")
-        self.device_file = w1_devices[0] + '/w1_slave' if len(w1_devices) > 0 else None
+        self.device_file = w1_devices[0] + "/w1_slave" if len(w1_devices) > 0 else None
 
     def read_temp_raw(self):
         if self.device_file != None:
@@ -189,7 +202,7 @@ class DS18B20(object):
             success = self.crc_check(lines)
 
             while not success and attempts < 3:
-                time.sleep(.2)
+                time.sleep(0.2)
                 lines = self.read_temp_raw()
                 success = self.crc_check(lines)
                 attempts += 1
@@ -198,8 +211,8 @@ class DS18B20(object):
                 temp_line = lines[1]
                 equal_pos = temp_line.find("t=")
                 if equal_pos != -1:
-                    temp_string = temp_line[equal_pos+2:]
-                    temp_c = float(temp_string)/1000.0
+                    temp_string = temp_line[equal_pos + 2 :]
+                    temp_c = float(temp_string) / 1000.0
 
             return temp_c
         else:
@@ -208,20 +221,36 @@ class DS18B20(object):
 
 class WindGauge(object):
     """ Object that represents a Wind Vane sensor. """
-    WIND_VANE_VOLTS = { 0.4: 0.0,   1.4: 22.5,  1.2: 45.0,  2.8: 67.5,
-                    2.7: 90.0,  2.9: 112.5, 2.2: 135.0, 2.5: 157.5,
-                    1.8: 180.0, 2.0: 202.5, 0.7: 225.0, 0.8: 247.5,
-                    0.1: 270.0, 0.3: 292.5, 0.2: 315.0, 0.6: 337.5}
+
+    WIND_VANE_VOLTS = {
+        0.4: 0.0,
+        1.4: 22.5,
+        1.2: 45.0,
+        2.8: 67.5,
+        2.7: 90.0,
+        2.9: 112.5,
+        2.2: 135.0,
+        2.5: 157.5,
+        1.8: 180.0,
+        2.0: 202.5,
+        0.7: 225.0,
+        0.8: 247.5,
+        0.1: 270.0,
+        0.3: 292.5,
+        0.2: 315.0,
+        0.6: 337.5,
+    }
     CM_IN_A_KM = 100000.0
     SECS_IN_AN_HOUR = 3600
-    def __init__(self,channel=0, anem_pin=5, anem_radius=9.0, anem_adjustment=1.18):
+
+    def __init__(self, channel=0, anem_pin=5, anem_radius=9.0, anem_adjustment=1.18):
         # pass channel of MCP3008 where wind vane is connected to
         self.adc = MCP3008(channel)
-        self.wind_count = 0 # Counts how many half-rotations
+        self.wind_count = 0  # Counts how many half-rotations
         self.last_wind_time = time.time()
         self.wind_speed_sensor = Button(anem_pin)
         self.wind_speed_sensor.when_pressed = self.spin
-        self.anemometer_radius_cm = anem_radius # Radius of your anemometer
+        self.anemometer_radius_cm = anem_radius  # Radius of your anemometer
         self.anemometer_adjustment = anem_adjustment
 
     # Every half-rotations, add 1 to count
@@ -235,7 +264,7 @@ class WindGauge(object):
     def get_wind_speed(self):
         """ Function that returns wind speed in km/hr. """
         wind_speed = self.calculate_speed(time.time() - self.last_wind_time)
-        self.reset_wind() # reset wind_count and last time reading
+        self.reset_wind()  # reset wind_count and last time reading
         return wind_speed
 
     def get_wind(self, length=5):
@@ -255,9 +284,9 @@ class WindGauge(object):
         return final_speed
 
     def read_direction(self):
-        wind = round(self.adc.value*3.3,1)
-        if not wind in self.WIND_VANE_VOLTS: # keep only good measurements
-            logdbg('Unknown Wind Vane value: %s' % str(wind))
+        wind = round(self.adc.value * 3.3, 1)
+        if not wind in self.WIND_VANE_VOLTS:  # keep only good measurements
+            logdbg("Unknown Wind Vane value: %s" % str(wind))
             return None
         else:
             return self.WIND_VANE_VOLTS[wind]
@@ -298,16 +327,16 @@ def get_average(angles):
 
     return 0.0 if average == 360 else average
 
+
 """ Section for testing purposes, so file can be run outside of weeWX.
     invoke this as follows from the weewx root dir:
     PYTHONPATH=bin python bin/weewx/drivers/byows_rpi.py"""
-if __name__ == '__main__':
+if __name__ == "__main__":
     station = ByowsRpiStation()
-    packet = {'dateTime': int(time.time() + 0.5),
-              'usUnits': weewx.METRIC}
+    packet = {"dateTime": int(time.time() + 0.5), "usUnits": weewx.METRIC}
 
     interval = 5
 
-    data = station.get_data(interval) # defaults to 5 seconds
+    data = station.get_data(interval)  # defaults to 5 seconds
     packet.update(data)
     print(packet)
